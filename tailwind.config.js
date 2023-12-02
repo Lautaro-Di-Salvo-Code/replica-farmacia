6f42379d9e7abae3c3360c6791e3f3f2f53a9033
primer push <lautarodisalvo5@gmail.com>
2023-12-02 07:43:31 -0300

    replica primer push

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "font-global": "'Roboto', sans-serif",
+      },
+      fontSize:{
+        "tamaño-letra-global":"1rem",
+      },
+      maxWidth:{
+        "maximo-ancho": "1200px",
+        "max-ancho-img":"100%"
+      }
+    },
+  },
+  plugins: [],
+}
+
