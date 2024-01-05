af2ba99cc90afe2649efb795323e4df1e862a39a
primer push <lautarodisalvo5@gmail.com>
2024-01-05 18:56:00 -0300

    button darklight

---
+++ src/script.js
@@ -0,0 +1,33 @@
+
+
+
+    let d = document
+    
+    let listapadre = d.querySelector('.options-father'),
+    buttonHam = d.querySelector('.hamburguer-darklight')
+    infoOptions = d.querySelector('.options-darklight')
+    
+    
+    console.log(buttonHam)
+    console.log(infoOptions)
+    
+    document.addEventListener('click', e =>{
+        
+        if(e.target.matches('.hamburguer-darklight') 
+        || e.target.matches('.hamburguer-darklight img')
+        || e.target.matches('.options-darklight')){
+            console.log(e.target)
+            infoOptions.classList.add('open')
+
+        } else {
+            infoOptions.classList.remove('open')
+
+        }
+        
+        
+        
+    })
+    
+    
+
+
